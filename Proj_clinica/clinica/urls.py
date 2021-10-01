99002b46bd9c09fcb3656ac3d75230d85ece41a1
mauricio <bestsound2u@gmail.com>
2021-10-01 12:03:06 -0300

    -criando o project clínica; -criando o app agendamento; -criando o app pacientes; -criando o app historicos; -criando o models; -criando a pasta api e add os arquivos serializers, viewsets; -configurando o sittings, urls;

---
+++ Proj_clinica/clinica/urls.py
@@ -0,0 +1,33 @@
+"""clinica URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import routers
+
+from pacientes.api.viewsets import PacientesViewSets
+from agendamentos.api.viewsets import AgendamentosViewsSet
+from historicos.api.viewsets import HistoricosViewset
+
+router = routers.DefaultRouter()
+router.register(r'pacientes', PacientesViewSets)
+router.register(r'agendamentos', AgendamentosViewsSet)
+router.register(r'historicos', HistoricosViewset)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls))
+]
